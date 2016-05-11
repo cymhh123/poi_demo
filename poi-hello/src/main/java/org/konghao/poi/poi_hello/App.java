a91b2e0ced2c9ced1919ace1ecbb739826269517
cymhh123 <cymhh123@126.com>
2016-05-11 19:52:58 +0800

    poi示例及封装
    poi示例及封装

---
+++ poi-hello/src/main/java/org/konghao/poi/poi_hello/App.java
@@ -0,0 +1,13 @@
+package org.konghao.poi.poi_hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
